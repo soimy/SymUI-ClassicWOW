0b3a5e99649b1215afe7824b53114fca75b2e61e
Shen Yiming <soimy@163.com>
2020-05-02 15:30:56 +0800

    新增MSBT和Tiptac的配置文件

---
+++ WTF/Account/YourUserName/SavedVariables/MikScrollingBattleText.lua
@@ -0,0 +1,135 @@
+
+MSBTProfiles_SavedVars = {
+	["profiles"] = {
+		["Default"] = {
+			["enableBlizzardDamage"] = true,
+			["creationVersion"] = "5.7.149",
+		},
+		["sym"] = {
+			["critOutlineIndex"] = 3,
+			["creationVersion"] = "5.7.149",
+			["critFontSize"] = 32,
+			["events"] = {
+				["NOTIFICATION_MONEY"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_ITEM_BUFF_FADE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_BUFF"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_LOOT"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_ALT_POWER_LOSS"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_POWER_LOSS"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CHI_CHANGE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_DEBUFF_STACK"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_ALT_POWER_GAIN"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_HOLY_POWER_FULL"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_ITEM_BUFF"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_HOLY_POWER_CHANGE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_POWER_GAIN"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_BUFF_FADE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_BUFF_STACK"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_AC_FULL"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_AC_CHANGE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_DEBUFF_FADE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CP_GAIN"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CURRENCY"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CP_FULL"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CHI_FULL"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_DEBUFF"] = {
+					["disabled"] = true,
+				},
+			},
+			["normalFontSize"] = 16,
+			["scrollAreas"] = {
+				["Outgoing"] = {
+					["scrollHeight"] = 150,
+					["offsetX"] = 315,
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -133,
+					["animationStyle"] = "Straight",
+					["direction"] = "Up",
+				},
+				["Incoming"] = {
+					["stickyDirection"] = "Up",
+					["scrollHeight"] = 150,
+					["offsetX"] = -353,
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -133,
+					["animationStyle"] = "Straight",
+					["direction"] = "Up",
+				},
+				["Static"] = {
+					["stickyDirection"] = "Up",
+					["direction"] = "Up",
+					["offsetX"] = -18,
+					["offsetY"] = 326,
+					["skillIconsDisabled"] = true,
+					["scrollHeight"] = 85,
+				},
+				["Notification"] = {
+					["stickyDirection"] = "Up",
+					["scrollHeight"] = 105,
+					["offsetX"] = -174,
+					["stickyBehavior"] = "Jiggle",
+					["offsetY"] = -103,
+					["skillIconsDisabled"] = true,
+					["direction"] = "Up",
+				},
+			},
+			["normalOutlineIndex"] = 3,
+			["enableBlizzardDamage"] = true,
+			["triggers"] = {
+				["MSBT_TRIGGER_EXECUTE"] = {
+					["disabled"] = true,
+				},
+			},
+		},
+	},
+}
+MSBT_SavedMedia = {
+	["fonts"] = {
+	},
+	["sounds"] = {
+	},
+}
---
+++ WTF/Account/YourUserName/SavedVariables/TipTac.lua
@@ -0,0 +1,86 @@
+
+TipTac_Config = {
+	["fontFace"] = "Fonts\\ARKai_T.ttf",
+	["fontSizeDelta"] = 2,
+	["anchorFrameTipPoint"] = "TOPLEFT",
+	["classification_elite"] = "+%s ",
+	["barHeight"] = 15,
+	["overrideFade"] = true,
+	["powerBar"] = false,
+	["tipBackdropEdge"] = "Interface\\Tooltips\\UI-Tooltip-Border",
+	["optionsLeft"] = 608.825378417969,
+	["backdropEdgeSize"] = 14,
+	["barTexture"] = "Interface\\AddOns\\VuhDo\\Images\\bar9.tga",
+	["showTarget"] = "none",
+	["classification_normal"] = "%s ",
+	["tipBackdropBG"] = "Interface\\Buttons\\WHITE8X8",
+	["anchorFrameUnitPoint"] = "TOPLEFT",
+	["tipColor"] = {
+		0.101960784313725, -- [1]
+		0.101960784313725, -- [2]
+		0.2, -- [3]
+		0.439042210578918, -- [4]
+	},
+	["hideDefaultBar"] = true,
+	["mouseOffsetX"] = 30,
+	["colRace"] = "|cffffffff",
+	["barFontFace"] = "Fonts\\ARHei.ttf",
+	["tipBorderColor"] = {
+		0.3, -- [1]
+		0.3, -- [2]
+		0.4, -- [3]
+		1, -- [4]
+	},
+	["mouseOffsetY"] = -20,
+	["colLevel"] = "|cffc0c0c0",
+	["healthBarClassColor"] = true,
+	["fontFlags"] = "",
+	["showGuildRank"] = false,
+	["reactColoredBorder"] = false,
+	["classification_minus"] = "-%s ",
+	["barFontFlags"] = "OUTLINE",
+	["hideWorldTips"] = true,
+	["anchorWorldTipType"] = "mouse",
+	["fadeTime"] = 0.1,
+	["fontSize"] = 13,
+	["classification_rare"] = "%s|r (Rare) ",
+	["optionsBottom"] = 508.904724121094,
+	["colorGuildByReaction"] = true,
+	["classification_rareelite"] = "+%s|r (Rare) ",
+	["left"] = 1422.11120605469,
+	["showTargetedBy"] = false,
+	["classification_worldboss"] = "%s|r (Boss) ",
+	["reactColoredBackdrop"] = false,
+	["anchorWorldUnitPoint"] = "TOPLEFT",
+	["classification_trivial"] = "~%s ",
+	["backdropInsets"] = 2.5,
+	["manaBar"] = false,
+	["colSameGuild"] = "|cffff32ff",
+	["anchorFrameTipType"] = "mouse",
+	["anchorWorldTipPoint"] = "TOPLEFT",
+	["colorNameByClass"] = false,
+	["gradientColor"] = {
+		0.8, -- [1]
+		0.8, -- [2]
+		0.8, -- [3]
+		0.2, -- [4]
+	},
+	["classColoredBorder"] = false,
+	["anchorWorldUnitType"] = "mouse",
+	["healthBarColor"] = {
+		0.3, -- [1]
+		0.9, -- [2]
+		0.3, -- [3]
+		1, -- [4]
+	},
+	["targetYouText"] = "<<YOU>>",
+	["healthBarText"] = "value",
+	["top"] = 576.825012207031,
+	["preFadeTime"] = 0.1,
+	["gradientTip"] = true,
+	["fontSizeDeltaSmall"] = -2,
+	["healthBar"] = true,
+	["barFontSize"] = 12,
+	["anchorFrameUnitType"] = "mouse",
+	["modifyFonts"] = false,
+}
